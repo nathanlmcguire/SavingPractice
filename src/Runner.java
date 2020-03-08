e3118b2cee08b3d1d744c97b1c35be2dca0c3f18
natha <natha@192.168.151.116>
2020-03-08 13:43:52 -0600

    Initial Commit

---
+++ src/Runner.java
@@ -0,0 +1,38 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Runner {
+
+	public static void main(String[] args) {
+		Scanner myScanner = new Scanner(System.in);
+		File myFile = new File("safePlace.txt");
+		FileWriter myWriter = null;
+		try {
+			 myWriter = new FileWriter(myFile);
+		} catch (IOException e) {
+			System.out.println("Uh Oh!");
+			System.exit(0);
+		}
+		String name = "";
+		
+		System.out.println("Hello world!");
+		System.out.println("What is your name?");
+		name = myScanner.nextLine();
+		System.out.println("Nice to meet you " + name + "!");
+		System.out.println("Don't worry, I'll remember it...");
+		
+		try {
+			myWriter.write(name);
+			myWriter.close();
+		} catch (IOException e) {
+			System.out.println("Uh Oh!");
+			System.exit(0);
+		}
+		
+
+	}
+
+}
